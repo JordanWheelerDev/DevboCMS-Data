190aa6be062f60bde5e2fac1fa0dd9b3308f5db9
Jordan Wheeler <163913618+JordanWheelerDev@users.noreply.github.com>
2024-06-30 13:38:12 -0230

    Create changelog.php

---
+++ changelog.php
@@ -0,0 +1,68 @@
+<div class="devbo-header-m mb-4">Changelog</div>
+                <div class="card rounded-0 border-0 mb-4">
+                    <div class="card-header rounded-0 devbo-card-header">[0.5.3] June 30th, 2024 Updates</div>
+                    <div class="card-body devbo-card-body">
+                        <ul>
+                            <li><span class="changelog-added">Added:</span>
+                                <ul>
+                                    <li>Configuration options to change the amount of credits earned while playing the
+                                        word solving puzzles.</li>
+                                </ul>
+                            </li>
+                            <li><span class="changelog-changed">Changed:</span>
+
+                                <ul>
+                                    <li>Nothing has been changed.</li>
+                                </ul>
+                            </li>
+                            <li><span class="changelog-removed">Removed:</span>
+
+                                <ul>
+                                    <li>Nothing has been removed.</li>
+                                </ul>
+                            </li>
+                            <li><span class="changelog-fixed">Fixed:</span>
+                                <ul>
+                                    <li>The puzzle input field now clears when correctly guessing the word.</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="card rounded-0 border-0 mb-4">
+                    <div class="card-header rounded-0 devbo-card-header">[0.5.2] June 29th, 2024 Updates</div>
+                    <div class="card-body devbo-card-body">
+                        <ul>
+                            <li><span class="changelog-added">Added:</span>
+                                <ul>
+                                    <li>Earn credits feature by playing a word puzzle game. Users can now earn credits
+                                        by solving word puzzles, each word if solved in under 5 seconds grants 1000
+                                        credits, if solved in 10 seconds or less grants 500 credits, if solved in 15
+                                        seconds or less 250 credits and 16 seconds or more grants 0 credits.</li>
+                                    <li>Online user count that dynamically updates every 10 seconds.</li>
+                                    <li>Online user display on the /online-users page that shows the "look" of the
+                                        online user, which updates dynamically every 10 seconds.</li>
+                                    <li>Credit leaderboard that displays user ranking by credits (who aren't
+                                        staff/ranked).</li>
+                                </ul>
+                            </li>
+                            <li><span class="changelog-changed">Changed:</span>
+
+                                <ul>
+                                    <li>Nothing has been changed.</li>
+                                </ul>
+                            </li>
+                            <li><span class="changelog-removed">Removed:</span>
+
+                                <ul>
+                                    <li>Nothing has been removed.</li>
+                                </ul>
+                            </li>
+                            <li><span class="changelog-fixed">Fixed:</span>
+                                <ul>
+                                    <li>Nothing has been fixed.</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
